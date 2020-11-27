c6bc9cdc026fa17af8b0a17642d768a98b6d8223
Raul Laredo Soria <Raul.Laredo@jalasoft.com>
2020-11-26 21:37:17 -0400

    Adding authentication

---
+++ auth-service/models/user.js
@@ -0,0 +1,35 @@
+const Joi = require('joi');
+
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  User.init({
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
+
+module.exports.UserValidationSchema = Joi.object().keys({
+  firstName: Joi.string().required(),
+  lastName: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required().min(6).max(32)
+});
